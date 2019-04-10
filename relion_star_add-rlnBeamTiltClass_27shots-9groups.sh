5cdfd0079a59b591d89a798ebb5c5473f5383f57
simonfromm <41924735+simonfromm@users.noreply.github.com>
2019-04-10 11:41:28 -0700

    rlnBeamTiltClass extension and fix

--- relion_star_add-rlnBeamTiltClass_27shots-9groups.sh
+++ relion_star_add-rlnBeamTiltClass_27shots-9groups.sh
@@ -26,6 +26,7 @@ if [ -z $1 ] ; then
  echo
  echo 'Script to add _rlnBeamTiltClass as a file to a relion star file'
  echo '     Tilt class is taken from last digits of rlnImageName (e.g. data recorded using SerialEM ImageShift)' 
+ echo '     Written for acquisition of a 3x3 hole matrix with 3 shots per hole; each hole will be the same BeamTiltClass' 
  echo
  echo "Usage ${0##*/} (1)"
  echo '(1) relion .star file which should be modified'
@@ -57,12 +58,12 @@ awk -v X=$HEADERLINES '{if(NR>X) print $0}' $STAR_IN > old_star_noheader.tmp
 awk -v X=$HEADERLINES '{if(NR<=X) print $0}' $STAR_IN > new_header.tmp
 
 i=$(( PARLINES + 1 ))
-echo "_rlnBeamTiltClass #${i}" > new_header.tmp
+echo "_rlnBeamTiltClass #${i}" >> new_header.tmp
 
 ###generate file with rlnBeamTiltClass
-#define column with _rlnImageName in CSPARC file
+#define column with _rlnImageName in star file
 IMGCOL=`cat $STAR_IN | grep _rlnImageName | awk '{print $2}' | sed -e 's/#//g'`
-awk -v X=${IMGCOL} '{print $X}' old_star_noheader.tmp | sed -e 's/.mrcs//g' -e 's/_/ /g' | awk '{printf "%i\n", $NF}' > rlnBeamTiltClass.tmp
+awk -v X=${IMGCOL} '{print $X}' old_star_noheader.tmp | sed -e 's/.mrcs//g' -e 's/_/ /g' | awk '{printf "%i\n", $NF}' | sed -e 's/\<2\>/1/g' -e 's/\<3\>/1/g' -e 's/\<5\>/4/g' -e 's/\<6\>/4/g' -e 's/\<8\>/7/g' -e 's/\<9\>/7/g' -e 's/\<11\>/10/g' -e 's/\<12\>/10/g' -e 's/\<14\>/13/g' -e 's/\<15\>/13/g' -e 's/\<17\>/16/g' -e 's/\<18\>/16/g' -e 's/\<20\>/19/g' -e 's/\<21\>/19/g' -e 's/\<23\>/22/g' -e 's/\<24\>/22/g' -e 's/\<26\>/25/g' -e 's/\<27\>/25/g' | sed -e 's/\<4\>/2/g' | sed -e 's/\<7\>/3/g' | sed -e 's/\<10\>/4/g' | sed -e 's/\<13\>/5/g' | sed -e 's/\<16\>/6/g' | sed -e 's/\<19\>/7/g' | sed -e 's/\<22\>/8/g' | sed -e 's/\<25\>/9/g' > rlnBeamTiltClass.tmp
 
 ###add rlnBeamTiltClass to old star file
 paste -d " " old_star_noheader.tmp rlnBeamTiltClass.tmp > new_star_noheader.tmp
