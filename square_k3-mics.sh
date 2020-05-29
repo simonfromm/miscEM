0bcfa4453b4d71ab020f1a59c71e7793448d5836
simonfromm <41924735+simonfromm@users.noreply.github.com>
2020-05-29 10:59:04 -0700

    square_k3-misc eman2 fix

--- square_k3-mics.sh
+++ square_k3-mics.sh
@@ -35,15 +35,7 @@ if [ -z $3 ] ; then
 fi
 
 ###load eman environment
-source ~/.initialize_eman2-sphire.sh
-#if [ -e /usr/local/software/EMAN2/bin/e2proc2d.py ]
-#then
-# export PATH=/usr/local/software/EMAN2/bin:$PATH
-# export LD_PRELOAD=/usr/local/software/EMAN2/lib/libmpi.so
-#else
-# export PATH=/usr/local/software/eman2/bin:$PATH
-# export LD_PRELOAD=/usr/local/software/eman2/lib/libmpi.so
-#fi
+source ~/.initialize_eman2-sphire.sh && conda activate
 
 ###set variables
 XDIM=$1
